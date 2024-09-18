23be6d251f5387b528f6898ffb966e2472032365
Rahul Singh <thesincererahul@gmail.com>
2024-09-18 15:27:27 +0530

    completed

--- README.md
+++ README.md
@@ -5,4 +5,7 @@
 ### > npm init -y
 ### > npm install express mysql2 bcrypt jsonwebtoken
 ### > mysql -u root 9936 sys<database.sql
-### > node app.js for run this project
+### > npm run dev or npm run start
+
+# Technology
+  1. Thunder Client available in VS CODE
--- index.js
+++ index.js
@@ -1,30 +1,32 @@
 const express = require('express');
 const app = express();
-const mysql = require('mysql2/promise');
 const bcrypt = require('bcrypt');
 // Bcrypt is a Node.js library that uses a cryptographic algorithm to securely hash passwords
 const jwt = require('jsonwebtoken');
+const mysql = require('mysql');
 
 // Database connection
 const db = mysql.createPool({
     host: 'localhost',
+    port: 3306,
     user: 'root',
     password: '9936',
-    database: 'sys'
+    database: 'irctc'
 });
+
 
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 // routes
-// 1. Register a user
+// Register a user
 app.post('/register', (req, res) => {
     const { name, email, password } = req.body;
     const hashedPassword = bcrypt.hashSync(password, 10);
     db.query('INSERT INTO users (name, email, password) VALUES (?, ?, ?)', [name, email, hashedPassword], (err, result) => {
         if (err) {
-            res.status(500).send({ message: 'Error registering user' });
+            res.status(500).send({ message: 'Failed' });
         } 
         else {
             res.send({ message: 'User registered successfully' });
@@ -33,7 +35,7 @@ app.post('/register', (req, res) => {
 });
 
 
-// 2. Login user
+// Login user
 app.post('/login',(req, res) => {
     const {email, password} = req.body;
     db.query('SELECT * FROM users WHERE email = ?',[email], (err, result) => {
@@ -55,12 +57,12 @@ app.post('/login',(req, res) => {
 });
 
 
-// 3. Add a new train
+// Add a new train
 app.post('/trains',(req, res)=> {
     const {name,source,destination,total_seats} = req.body;
     db.query('INSERT INTO trains(name,source,destination,total_seats,available_seats) VALUES(?,?,?,?,?)',[name,source,destination,total_seats,total_seats], (err, result) => {
         if(err) {
-            res.status(500).send({message: 'Error adding train'});
+            res.status(500).send({message: 'Failed'});
         }
         else {
             res.send({message: 'Train added successfully'});
@@ -69,12 +71,12 @@ app.post('/trains',(req, res)=> {
 });
 
 
-// 4. get seat availability
+// get seat availability
 app.get('/trains',(req, res) => {
     const {source, destination} = req.query;
     db.query('SELECT * FROM trains WHERE source = ? AND destination = ?', [source, destination],(err, result) => {
         if(err) {
-            res.status(500).send({message:'Error fetching trains'});
+            res.status(500).send({message:'Failed'});
         }
         else {
             req.send({trains: result});
@@ -82,7 +84,7 @@ app.get('/trains',(req, res) => {
     });
 });
 
-// 5. book a seat
+// book a seat
 app.post('/bookings', (req, res) => {
     const{train_id} = req.body;
     db.query('SELECT available_seats FROM trains WHERE id=?',[train_id], (err, result)=>{
@@ -92,12 +94,12 @@ app.post('/bookings', (req, res) => {
         else {
             db.query('UPDATE trains SET available_seats = available_seats-1 WHERE id =?',[train_id],(err, result)=>{
                 if(err) {
-                    res.status(500).send({message: 'Error booking seat'});
+                    res.status(500).send({message: 'Failed'});
                 }
                 else {
                     db.query('INSERT INTO bookings(user_id, train_id) VALUES(?,?)',[req.user.id, train_id],(err,result)=> {
                         if(err) {
-                            res.status(500).send({message: 'Error booking seat'});
+                            res.status(500).send({message: 'Failed'});
                         }
                         else {
                             res.send({message: 'seat booked successfully'});
@@ -110,7 +112,7 @@ app.post('/bookings', (req, res) => {
 });
 
 
-// 6. get specific booking details
+// get specific booking details
 app.get('/bookings/:booking_id',(req, res)=>{
     const {booking_id} = req.params;
     db.query('SELECT * FROM bookings WHERE id = ?',[booking_id],(err,result)=>{
@@ -124,8 +126,9 @@ app.get('/bookings/:booking_id',(req, res)=>{
 })
 
 // Start server
-const port = 3000;
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 5000;
+
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
 });
-console.log("Hello")
